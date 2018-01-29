bc3887943929c59cab3e280b243d1111a9fecc86
tiandiesel <guardor@163.com>
2018-01-29 07:33:17 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
