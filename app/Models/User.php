38e9a80f774dbda4aed64c75c15d43640e3d9032
tiandiesel <guardor@163.com>
2018-01-30 03:19:08 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,8 @@ class User extends Authenticatable
     {
         return $this->id == $model->user_id;
     }
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
