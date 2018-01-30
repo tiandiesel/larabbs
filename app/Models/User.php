0af2e619621852ca1a5725c9955197dd54c1261b
tiandiesel <guardor@163.com>
2018-01-30 01:39:46 +0000

    Author can delete topic

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,4 +35,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(Topic::class);
     }
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
