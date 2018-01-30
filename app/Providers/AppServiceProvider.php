e2df68fc89240a189dfcedccaa6787bee854a587
tiandiesel <guardor@163.com>
2018-01-30 08:00:05 +0000

    Sidebar links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,15 +11,16 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-	{
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
-		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
-		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+     public function boot()
+     {
+         \App\Models\User::observe(\App\Observers\UserObserver::class);
+         \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
-        //
-        \Carbon\Carbon::setLocale('zh');
-    }
+         // Carbon 中文化配置
+         \Carbon\Carbon::setLocale('zh');
+     }
 
     /**
      * Register any application services.
